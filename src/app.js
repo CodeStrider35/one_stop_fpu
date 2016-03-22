2c6945b2dcc4c674cd08215bc0b66498f48519dc
CodeStrider35 <prince1moreno@gmail.com>
2016-03-22 10:15:08 -0700

    preparing for heroku launch

--- src/app.js
+++ src/app.js
@@ -2,14 +2,9 @@ var express = require('express');
 
 var app = express();
 
-app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/src/public/index.html', function (req, res){
-    res.render()
-});
-
-var port = process.env.PORT || 3000;
-
-app.listen(port, function(){
-    console.log("Listening on localhost 3000");
+var server = app.listen(process.env.PORT || 3000, function(){
+    var port = server.address().port;
+    console.log("App now running on port", port);
 });
